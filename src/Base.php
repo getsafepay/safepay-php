3c87c58185dcd86ad9cb1e5df03383574d76df09
Matiullah <mati_ullah31@yahoo.com>
2022-06-23 00:18:50 +0500

    setup

--- src/Base.php
+++ src/Base.php
@@ -7,13 +7,13 @@ namespace SafePay;
 abstract class Base {
 
 
-	const SANDBOX                     = "sandbox";
-    const PRODUCTION                  = "production";
+    const SANDBOX  = "sandbox";
+    const PRODUCTION = "production";
 
     const CURRENCY = 'PKR';
 
-    const PRODUCTION_CHECKOUT_URL     = "https://www.getsafepay.com/components";
-    const SANDBOX_CHECKOUT_URL        = "https://sandbox.api.getsafepay.com/components";
+    const PRODUCTION_CHECKOUT_URL = "https://www.getsafepay.com/components";
+    const SANDBOX_CHECKOUT_URL  = "https://sandbox.api.getsafepay.com/components";
 
     public static $init_transaction_endpoint = "order/v1/init";
 
